358ceeb5bb0de822d70bbd073b31a3cdc95a9fe8
rp-guruh <github.guruh@gmail.com>
2021-08-23 14:13:01 +0700

    first commit

---
+++ app/Main/SideMenu.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Main;
+
+class SideMenu
+{
+    /**
+     * List of side menu items.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public static function menu()
+    {
+        return [
+            'dashboard' => [
+                'icon' => 'home',
+                'title' => 'Dashboard',
+                'route_name' => 'dashboard-overview-2',
+                'params' => [
+                    'layout' => 'side-menu',
+                ],
+            ],
+                
+           
+            'file-manager' => [
+                'icon' => 'hard-drive',
+                'route_name' => 'file-manager',
+                'params' => [
+                    'layout' => 'side-menu'
+                ],
+                'title' => 'File Laporan Keuangan'
+            ],
+           
+            'devider',
+            
+            'profile' => [
+                'icon' => 'trello',
+                'title' => 'Lihat Profil',
+                'route_name' => 'profile-overview-1',
+                'params' => [
+                    'layout' => 'side-menu'
+                ],
+            ],
+            
+        ];
+    }
+}
